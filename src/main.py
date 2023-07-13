703df1ee78c9958a1e339d69526799dd9c644f52
jcguidry <48544496+jcguidry@users.noreply.github.com>
2023-07-13 01:14:46 -0500

    configured deployment attempt 1

--- src/main.py
+++ src/main.py
@@ -1,14 +1,14 @@
 import base64
 import functions_framework
 
-from src.ingest_flight_snapshots import main
+from function.ingest_flight_snapshots import main
 
 # Triggered from a message on a Cloud Pub/Sub topic.
 @functions_framework.cloud_event
-def hello_pubsub(cloud_event):
+def ingest(cloud_event):
     # Print out the data from Pub/Sub, to prove that it worked
-    main()
-    
+    # main()
+
     print(base64.b64decode(cloud_event.data["message"]["data"]))
     
     
