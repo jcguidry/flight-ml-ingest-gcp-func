5657f711090ecc7ae574844954603d42b6d07955
jcguidry <48544496+jcguidry@users.noreply.github.com>
2023-07-14 02:11:31 -0500

    final deploy

--- src/main.py
+++ src/main.py
@@ -9,6 +9,6 @@ def ingest(cloud_event):
     # Print out the data from Pub/Sub, to prove that it worked
     main()
 
-    print(base64.b64decode(cloud_event.data["message"]["data"]))
+    # print(base64.b64decode(cloud_event.data["message"]["data"]))
     
     
